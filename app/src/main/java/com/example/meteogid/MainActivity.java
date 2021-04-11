f31ee269d4f40e038f5d05103d0d932c28bd91d5
Ivan <ivan.polyakov.2001@mail.ru>
2021-04-11 15:23:05 +0300

    Добавлен парсер температуры. ЙА ДЖАВА-ПРОГРАММИСТ

--- app/src/main/java/com/example/meteogid/MainActivity.java
+++ app/src/main/java/com/example/meteogid/MainActivity.java
@@ -2,16 +2,31 @@ package com.example.meteogid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    EditText etText;
+    TextView waiting;
+    EditText city;
     Button btnSaveCity;
     SharedPreferences sPref;
 
@@ -22,12 +37,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etText = (EditText)findViewById(R.id.etText);
+        waiting = (TextView)findViewById(R.id.Waiting);
+        city = (EditText)findViewById(R.id.city);
 
         btnSaveCity = (Button)findViewById(R.id.btnSaveCity);
         btnSaveCity.setOnClickListener(this);
 
         loadtext();
+        showdata();
     }
 
 
@@ -35,13 +52,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v.getId() == R.id.btnSaveCity){
             savetext();
+            showdata();
         }
     }
 
     private void savetext() {
         sPref = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor ed = sPref.edit();
-        ed.putString(SavedCity, etText.getText().toString());
+        ed.putString(SavedCity, city.getText().toString());
         ed.commit();
         Toast.makeText(MainActivity.this, "Город сохранён", Toast.LENGTH_SHORT).show();
     }
@@ -49,7 +67,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void loadtext() {
         sPref = getPreferences(MODE_PRIVATE);
         String SavedText = sPref.getString(SavedCity, "");
-        etText.setText(SavedText);
+        city.setText(SavedText);
 
+    }
+
+    public void showdata(){
+        String City = city.getText().toString();
+        String key = "a1f886dec466dde0dd09b3f75fa9455d";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=" + key + "&units=metric&lang=ru";
+
+        new GetURLData().execute(url);
+
+    }
+
+    private class GetURLData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            waiting.setText("Ожидайте...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+
+                return buffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                waiting.setText("Температура: " + jsonObject.getJSONObject("main").getDouble("temp"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
