2a081a6e4f9eaa2eb14e8059078fb2b42dce3bee
Ivan <ivan.polyakov.2001@mail.ru>
2021-04-29 21:45:31 +0300

    Я смог нормализовать дату. НАКОНЕЦ, ЗА 6 ЧАСОВ

--- app/src/main/java/com/example/meteogid/MainActivity.java
+++ app/src/main/java/com/example/meteogid/MainActivity.java
@@ -17,15 +17,18 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Objects;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity{
 
     ImageView pogoda;
-    TextView waiting;
+    TextView waiting, sit1, sit2, sit3, sit4, sit5, sit6, temp1, temp2, temp3, temp4, temp5, temp6;
     EditText city;
     Button btnSaveCity, more;
     SharedPreferences sPref;
@@ -127,7 +130,6 @@ public class MainActivity extends AppCompatActivity{
                 // Отсюда нужно брать координаты введённого города и его название
                 String href1 = "https://api.openweathermap.org/data/2.5/weather?q="+city.getText()+"&appid=a1f886dec466dde0dd09b3f75fa9455d&units=metric&lang=ru";
                 JSONObject Json=JsonReader.readJsonFromUrl(href1);
-                System.out.println(Json);
 
                 //Парсинг координат
                 double lat = Json.getJSONObject("coord").getDouble("lat");
@@ -137,7 +139,6 @@ public class MainActivity extends AppCompatActivity{
                 // Основной API с ситуацией и прогнозами
                 String href2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,alerts&appid=a1f886dec466dde0dd09b3f75fa9455d&lang=ru&units=metric";
                 JSONObject Json2 = JsonReader.readJsonFromUrl(href2);
-                System.out.println(Json2);
 
                 return Json2;
 
@@ -162,22 +163,60 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-
-
-
             try {
-
-                String time;
-                SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
-                dateFormat.setTimeZone(TimeZone.getTimeZone(jsonObject.getString("timezone")));
-                time = dateFormat.format(new Date());
-
-
 
                 currentTemp= (int) jsonObject.getJSONObject("current").getDouble("temp");
                 currentPreasure = (int) jsonObject.getJSONObject("current").getDouble("pressure");
                 currentWet = (int) jsonObject.getJSONObject("current").getDouble("humidity");
                 currentsit= jsonObject.getJSONObject("current").getJSONArray("weather").getJSONObject(0).getString("description");
+
+                Long unix = jsonObject.getJSONObject("current").getLong("dt");
+                String zone = jsonObject.getString("timezone");
+                int tz_off = jsonObject.getInt("timezone_offset");
+
+                Date date = new Date(unix*1000L);
+                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm Z");
+                sdf.setTimeZone(TimeZone.getTimeZone(zone));
+
+                
+                String formattedDate = sdf.format(date);
+                System.out.println(formattedDate);
+
+
+
+                sit1 = (TextView) findViewById(R.id.sit1);
+                temp1 = (TextView) findViewById(R.id.temp1);
+                sit1.setText("Завтра: " + jsonObject.getJSONArray("daily").getJSONObject(1).getJSONArray("weather").getJSONObject(0).getString("description"));
+                temp1.setText(String.valueOf( (int) jsonObject.getJSONArray("daily").getJSONObject(1).getJSONObject("temp").getDouble("day")) + "°C");
+
+                sit2 = (TextView) findViewById(R.id.sit2);
+                temp2 = (TextView) findViewById(R.id.temp2);
+                sit2.setText("Завтра: " + jsonObject.getJSONArray("daily").getJSONObject(2).getJSONArray("weather").getJSONObject(0).getString("description"));
+                temp2.setText(String.valueOf( (int) jsonObject.getJSONArray("daily").getJSONObject(2).getJSONObject("temp").getDouble("day")) + "°C");
+
+                sit3 = (TextView) findViewById(R.id.sit3);
+                temp3 = (TextView) findViewById(R.id.temp3);
+                sit3.setText("Завтра: " + jsonObject.getJSONArray("daily").getJSONObject(3).getJSONArray("weather").getJSONObject(0).getString("description"));
+                temp3.setText(String.valueOf( (int) jsonObject.getJSONArray("daily").getJSONObject(3).getJSONObject("temp").getDouble("day")) + "°C");
+
+                sit4 = (TextView) findViewById(R.id.sit4);
+                temp4 = (TextView) findViewById(R.id.temp4);
+                sit4.setText("Завтра: " + jsonObject.getJSONArray("daily").getJSONObject(4).getJSONArray("weather").getJSONObject(0).getString("description"));
+                temp4.setText(String.valueOf( (int) jsonObject.getJSONArray("daily").getJSONObject(4).getJSONObject("temp").getDouble("day")) + "°C");
+
+                sit5 = (TextView) findViewById(R.id.sit5);
+                temp5 = (TextView) findViewById(R.id.temp5);
+                sit5.setText("Завтра: " + jsonObject.getJSONArray("daily").getJSONObject(5).getJSONArray("weather").getJSONObject(0).getString("description"));
+                temp5.setText(String.valueOf( (int) jsonObject.getJSONArray("daily").getJSONObject(5).getJSONObject("temp").getDouble("day")) + "°C");
+
+                sit6 = (TextView) findViewById(R.id.sit6);
+                temp6 = (TextView) findViewById(R.id.temp6);
+                sit6.setText("Завтра: " + jsonObject.getJSONArray("daily").getJSONObject(6).getJSONArray("weather").getJSONObject(0).getString("description"));
+                temp6.setText(String.valueOf( (int) jsonObject.getJSONArray("daily").getJSONObject(6).getJSONObject("temp").getDouble("day")) + "°C");
+
+
+
+
             } catch (JSONException e) {
                 System.out.println("Ошибка парсинга");
             }
@@ -191,6 +230,7 @@ public class MainActivity extends AppCompatActivity{
                     break;
                 case ("малооблачно"):
                 case ("облачно с прояснениями"):
+                case ("переменная облачность"):
                     pogoda.setImageResource(R.drawable.smallcloudy);
                     break;
                 case ("небольшой снег"):
@@ -200,6 +240,7 @@ public class MainActivity extends AppCompatActivity{
                     pogoda.setImageResource(R.drawable.snow);
                     break;
                 case ("небольшой дождь"):
+                case ("снег с дождём"):
                     pogoda.setImageResource(R.drawable.smallrain);
                     break;
                 case ("дождь"):
@@ -209,7 +250,7 @@ public class MainActivity extends AppCompatActivity{
                 break;
             }
 
-          waiting.setText(String.valueOf(currentsit)+"\n"+String.valueOf(currentTemp)+"°С");
+          waiting.setText("Сегодня: \n" + String.valueOf(currentsit)+",\n"+String.valueOf(currentTemp)+"°С");
 
             //Toast.makeText(MainActivity.this, "Найденное место: " + name, Toast.LENGTH_LONG).show();
 
